06a8dadf980477be79d7e22126e9215a7f4209b3
Daniel <dmarcelino@live.com>
2015-07-12 17:16:33 -0300

    pre release

--- R/fptp2av.R
+++ R/fptp2av.R
@@ -1,3 +1,68 @@
+#' @encoding UTF-8
+#' @title Simulate election outcomes for AV given FPTP votes.
+#'
+#' @description A program to simulate election outcomes under the Alternative Vote System using the actual First-Past-The-Post votes.
+#'
+#' @param data A flat table or data frame containing FPTP votes for each district.
+#' @param link A link function containing a structure for candidates/parties relations.
+#'
+#' @return A text file \code{(csv)} containing simulated electoral results for each district.
+#' @references
+#' Marcelino, Daniel (2013). \emph{SciencesPo: A Tool Set for Analyzing Political Behaviour Data}. Available at SSRN: \url{http://dx.doi.org/10.2139/ssrn.2320547}
+#'
+#' @note Unfortunately, this function is not so flexible regarding the data supplied. For it to work properly, the input table should have  the same format (flat table) as the example data provided within this package. Namely, the same structure with an Id column, the name of the constituencies in another column, and each party achievements distributed in columns.  Besides the dataset, a "link" structure for party/candidates' relations is also necessary.
+#'
+#' @export
+#'
+#' @example
+#' # Feeding data:
+#' data(ge2010)
+#'
+#' # Let's take a small portion of the data.
+#' IR <-ge2010[ge2010$Region=="Northern Ireland",]
+#'
+#' # First, we create vectors for parties/candidates link
+#' # For simplicity, I'm considering only parties which received some votes.
+#'
+#' APNI<- c("SF", "DUP", "SDLP", "TUV", "UCUNF")
+#' BNP<-c("Ch P", "Con", "CPA", "NF", "UKIP");
+#' Ch_P<-c("BNP", "CPA", "Con", "ED")
+#' Con<-c("LD", "UKIP", "ED", "Ch P", "CPA")
+#' CPA<-c("BNP", "Ch P", "Con", "ED")
+#' DUP<-c("SDLP", "TUV", "UCUNF", "APNI")
+#' ED<-c("Con", "BNP", "NF", "CPA", "Ch P")
+#' Grn<-c("LD", "Lab", "SDLP", "PC", "SSP", "TUSC", "TUV")
+#' Lab<-c("LD", "Grn", "SDLP", "Soc", "SSP", "TUSC", "TUV")
+#' LD<-c("Lab", "Con", "Grn", "PC", "SDLP")
+#' MRLP<-c("Con", "Lab", "LD", "Grn", "PC", "SDLP", "SNP", "SSP", "UKIP")
+#' NF<-c("BNP", "Con", "ED", "UKIP")
+#' PC<-c("Con", "Lab", "LD")
+#' Respect<-c("Con", "Lab", "LD")
+#' SDLP<-c("Grn", "Lab", "LD")
+#' SF<-c("APNI", "USUNF")
+#' Soc<-c("Lab", "SSP", "SDLP", "TUSC", "TUV")
+#' SNP<-c("Ch P", "CPA", "UKIP")
+#' SSP<-c("Grn", "Lab", "LD", "SDLP", "TUSC", "TUV")
+#' TUSC<-c("SDLP", "Soc", "SSP", "TUV")
+#' TUV<-c("SDLP", "Soc", "SSP", "TUSC")
+#' UCUNF<-c("APNI", "SF", "DUP")
+#' UKIP<-c("BNP", "NF", "Con")
+#'
+#' ## vector with party/candidate relations:
+#'
+#' party_chains <-structure(list(APNI,BNP,Ch_P,Con,CPA,DUP,ED,
+#' Grn, Lab,LD,MRLP,NF,PC,Respect,SDLP,SF,Soc,SNP,SSP,TUSC,TUV,UCUNF,
+#'  UKIP), .Names = c("APNI","BNP","Ch_P","Con","CPA","DUP","ED","Grn",
+#'  "Lab","LD","MRLP","NF","PC","Respect","SDLP","SF","Soc","SNP","SSP",
+#'  "TUSC", "TUV","UCUNF","UKIP" ));
+#'
+#' ## Run the algorithm
+#' \dontrun{fptp2av(data=IR, link=party_chains)}
+#'
+#' @keywords Simulations
+#' @importFrom data.table data.table :=
+#' @importFrom utils write.csv
+#' @importFrom stats na.omit complete.cases
 fptp2av <-
 function(data=NULL, link=NULL){
 	if(is.null(data)){
@@ -10,7 +75,7 @@ function(data=NULL, link=NULL){
 		else{
 	start_time<-(Sys.time())
 	new1<-NULL;candidates<-NULL;vote_spread<-NULL;
-	
+
 	.District <-
 	function(name, electorate, each_row, link, max_vote_length){
 		votes<-list()
@@ -35,7 +100,7 @@ function(data=NULL, link=NULL){
 				ballot[[1]]<-as.character(each_row[candid,candidates])
 				votes_1<-rep(list(c(ballot)), as.numeric(as.character(each_row[candid,vote_spread])))
 			}
-		
+
 	    # votes_1<-rep(list(c(ballot)), as.numeric(as.character(each_row[candid,vote_spread])))
 	    votes<-c(votes,list(votes_1))
 		names(votes)<-c(names(votes)[1:(length(votes)-1) ], as.character(each_row[candid,candidates]))
@@ -175,8 +240,7 @@ function(start_time) {
 }
 
 
-column.names<-c("District", "Turnout", "Outcome", "FPTP.Winner", "FPTP.Winner.Votes",
-	 "AV.Winner", "AV.Winner.Votes")
+column.names<-c("District", "Turnout", "Outcome", "FPTP.Winner", "FPTP.Winner.Votes", "AV.Winner", "AV.Winner.Votes")
 
 data.to.read<-data.table::data.table(data)
 parties<-names(data.to.read)[c(7:ncol(data.to.read))]
@@ -213,9 +277,10 @@ if(fptp_winner==av_winner){outcome<-"Unaltered"} 	else{outcome<-"Altered"}
 	newrow<-c(name,toString(electorate),outcome,fptp_winner,toString(fptp_votes),av_winner,toString(av_votes),toString(av_counter))
 	data.to.write<-rbind(data.to.write,c(newrow))
 	message("Done at ", .timediff(start_time))
-	} 
+	}
 outfile <- paste("Sim_data_in",format(Sys.time(),".%d.%b.%Y-%H:%M:%S"),".csv",sep="")
 write.csv(data.to.write, file=outfile, row.names=FALSE)
 		}
 	}
 }
+NULL
